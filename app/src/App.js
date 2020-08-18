6b25e139bd3ecea8654626c1520b782198a51882
William Fullerton <wrk.fullerton@gmail.com>
2020-08-18 18:39:42 -0500

    changes made to app / added UserAuth

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
+import UserAuth from './components/UserAuth';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+    <Router>
+      <Switch>
+        
+        <Route path="/">
+          <Header />
+          <UserAuth/>
+        </Route>
+
+        </Switch>
+    </Router>
+      
     </div>
   );
 }
