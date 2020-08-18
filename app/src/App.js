9d2d68227923b126429bdf9517e31520c237360c
William Fullerton <wrk.fullerton@gmail.com>
2020-08-18 18:29:41 -0500

    starter code with context API setup with firebase.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2>Tutoring App</h2>
+      <Header />
     </div>
   );
 }
---
+++ app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            
+                <h2 className="header__text">TutorNet</h2>
+            
+        </div>
+    )
+}
+
+export default Header
