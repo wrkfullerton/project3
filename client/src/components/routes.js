9524ee8daef4733f4c4e71c970946c87ef6dfa67
Eddie Sevilla <esevilla93@gmail.com>
2020-09-03 21:15:19 -0500

    changes

--- client/src/components/routes.js
+++ client/src/components/routes.js
@@ -1,35 +1,76 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const mysql = require("mysql");
+var app = express();
+// app.use(bodyparser.json());
 
-const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "sevilla08",
-    database: "userdb"
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'sevilla08',
+    database: 'userdb',
+    multipleStatements: true
 });
 
-const app = express();
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('Connection Established Successfully');
+    else
+        console.log('Connection Failed!' + JSON.stringify(err, undefined, 2));
+});
+
+//Establish the server connection
+//PORT ENVIRONMENT VARIABLE
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
+
+//Creating GET Router to fetch all the learner details from the MySQL Database
+app.get('/users', (req, res) => {
+    mysqlConnection.query('SELECT * FROM Users', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+
+
+// const connection = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "sevilla08",
+//     database: "userdb"
+// });
+
 
 //console is showing message
 //too many different routes and folders cant find the correct route to populate mysql data into console
 
 
-app.get("/Users", function (req, res) {
+// app.get("/", (req, res) => {
+//     db.Users.findAll()
+//         .then(menu => {
+//             res.send(menu)
+//         })
+//         .catch(err => console.log(err));
+// });
 
-    connection.getConnection(function (err, connection) {
+//     connection.getConnection(function (err, connection) {
 
 
 
-        connection.query("SELECT * FROM Users", function (error, results, fields) {
+//         connection.query("SELECT * FROM Users", function (error, results, fields) {
 
 
-            if (error) throw error;
-            res.send(results)
-        });
-    });
-});
+//             if (error) throw error;
+//             res.send(results)
+//         });
+//     });
+// });
 
-app.listen(3000, () => {
-    console.log("go to localhost");
-});
+// app.listen(3000, () => {
+//     console.log(results);
+// });
+
