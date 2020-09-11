308d8cca798a3bc23ead96b7bfa92cd22802a61b
Seulki Lee <seulki.lee.0428@gmail.com>
2020-09-10 19:40:33 -0500

    adjust mysql

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,18 @@ DELETE FROM Users;
 
 -- resets the auto increment to 1 if row id doesnt start at 1
 ALTER TABLE Users AUTO_INCREMENT = 1;
+
+CREATE TABLE User (
+   id INT NOT NULL AUTO_INCREMENT,
+   first_name VARCHAR(100),
+   last_name VARCHAR(100),
+   email VARCHAR(100),
+   password VARCHAR(100),
+   profile VARCHAR(100),
+   grade VARCHAR(100),
+   subject VARCHAR(100),
+   createdAt DATE,
+  
+   PRIMARY KEY (id)
+);
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,34 +1,34 @@
-USE userDB;
+USE userdb;
 
-INSERT INTO users (id, first_name, last_name, email, password, profile, grade, subject, created)
+INSERT INTO User (first_name, last_name, email, password, profile, grade, subject, createdAt)
 
 VALUES 
 
-("0", "Vidal", "Salazar", "v.salazar@tutor.net","password100", "teacher", "High", "math", now()),
+( "Vidal", "Salazar", "v.salazar@tutor.net","password100", "teacher", "High", "math", now()),
 
-("0", "James", "Martinez", "j.martinez@tutor.net", "password101", "teacher", "Middle", "math", now()),
+( "James", "Martinez", "j.martinez@tutor.net", "password101", "teacher", "Middle", "math", now()),
 
-("0", "Robert", "Vera", "r.vera@tutor.net", "password102", "teacher", "Elementary", "math", now()),
+( "Robert", "Vera", "r.vera@tutor.net", "password102", "teacher", "Elementary", "math", now()),
 
-("0", "Jack", "Davis", "j.davis@tutor.net", "password103", "teacher", "High", "history", now()),
+( "Jack", "Davis", "j.davis@tutor.net", "password103", "teacher", "High", "history", now()),
 
-("0", "Brandon", "Rodriguez", "b.rodriguez@tutor.net", "password104", "teacher", "Middle", "History", now()),
+( "Brandon", "Rodriguez", "b.rodriguez@tutor.net", "password104", "teacher", "Middle", "History", now()),
 
-("0", "Kyle", "Mendoza", "k.mendoza@tutor.net", "password105", "teacher", "Elementary", "History", now()),
+( "Kyle", "Mendoza", "k.mendoza@tutor.net", "password105", "teacher", "Elementary", "History", now()),
 
-("0", "Andrea", "Pellecer", "a.pellecer@tutor.net", "password105", "teacher", "High", "English", now()),
+( "Andrea", "Pellecer", "a.pellecer@tutor.net", "password105", "teacher", "High", "English", now()),
 
-("0", "Desiree", "Aranda", "d.aranda@tutor.net", "password106", "teacher", "Middle", "English", now()),
+( "Desiree", "Aranda", "d.aranda@tutor.net", "password106", "teacher", "Middle", "English", now()),
 
-("0", "Fernando", "Penaloza", "f.penaloza@tutor.net", "password107", "teacher", "Elementary", "English", now()),
+( "Fernando", "Penaloza", "f.penaloza@tutor.net", "password107", "teacher", "Elementary", "English", now()),
 
-("0", "Nathan", "Henderson", "n.henderson@tutor.net", "password108", "teacher", "High", "Science", now()),
+( "Nathan", "Henderson", "n.henderson@tutor.net", "password108", "teacher", "High", "Science", now()),
 
-("0", "Luke", "Duvall", "l.duvall@tutor.net", "password109", "teacher", "Middle", "Science", now()),
+( "Luke", "Duvall", "l.duvall@tutor.net", "password109", "teacher", "Middle", "Science", now()),
 
-("0", "Ben", "Aguilar", "b.aguilar@tutor.net", "password110", "teacher", "Elementary", "Science", now()),
+( "Ben", "Aguilar", "b.aguilar@tutor.net", "password110", "teacher", "Elementary", "Science", now()),
 
-("0", "Celeste", "Guzman", "c.guzman@tutor.net", "password111", "teacher", "Middle", "Spanish", now())
+( "Celeste", "Guzman", "c.guzman@tutor.net", "password111", "teacher", "Middle", "Spanish", now())
 
 
 -- INSERT INTO students (student, grade, subject, email)
