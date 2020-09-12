c57e43b60496c2154d289c682675f0387c135dab
William Fullerton <wrk.fullerton@gmail.com>
2020-09-12 09:17:26 -0500

    added .gitignore / and hid API keys

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -9,12 +9,12 @@ function SearchPage({tutors}) {
             <div className="searchPage__info">
                 <h5><strong>{tutors.length}</strong> Tutors Available</h5>
                 
-                <Button variant="outlined">Type of Tutor</Button>
+                {/* <Button variant="outlined">Type of Tutor</Button>
                 <Button variant="outlined">Tutor Rating</Button>
                 <Button variant="outlined">Price Range</Button>
                 <Button variant="outlined">In Person or Virtual</Button>
                 
-                <Button variant="outlined">More Filters</Button>
+                <Button variant="outlined">More Filters</Button> */}
 
                 {tutors.map((tutor) => (
                     <SearchResult 
